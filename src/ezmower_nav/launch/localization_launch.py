940e6e532ab4ac807cc35ddb3a398766309ca210
bcevikel <bcevikel19@ku.edu.tr>
2024-05-23 21:42:15 +0300

    changed map

--- src/ezmower_nav/launch/localization_launch.py
+++ src/ezmower_nav/launch/localization_launch.py
@@ -64,7 +64,7 @@ def generate_launch_description():
 
         DeclareLaunchArgument(
             'map',
-            default_value=os.path.join(bringup_dir, 'maps', 'arena3by2v1.yaml'),
+            default_value=os.path.join(bringup_dir, 'maps', 'beko_house.yaml'),
             description='Full path to map yaml file to load'),
 
         DeclareLaunchArgument(
